0258335891abfeeb6e9e72cdfbea040eb2999559
dnjsxorla <32029676+dnjsxorla@users.noreply.github.com>
2022-02-15 00:53:26 +0900

    Update test02.py

--- test02.py
+++ test02.py
@@ -1,3 +1,5 @@
+#-*-coding:utf-8-*-
+
 import hmac
 import hashlib
 import binascii
@@ -6,16 +8,17 @@ import time
 import requests
 import json
 import urllib.parse
+from ast import literal_eval
 
 class Coupang:
 
 #Coupang default value Access_ket and Secret_key and etc... 
     def __init__(self):
-        self.access_key = 'x' 
-        self.secret_key = 'x'
+        self.access_key = 'X' 
+        self.secret_key = 'X'
         self.subId = 'AF8339687' 
         self.method = 'GET' 
-        self.limit = '10'
+        self.limit = '3'
         self.DOMAIN = 'https://api-gateway.coupang.com' 
         
     def generateHmac(self, url): 
@@ -31,26 +34,61 @@ class Coupang:
         url = "{}{}".format(self.DOMAIN, URL)
         response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
         resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
-        print(resdata)
-        return 0
+        #print(resdata)
+        return resdata
 
     def best_categories_coupang(self, Id):
         URL = "/v2/providers/affiliate_open_api/apis/openapi/products/bestcategories/"+ Id + "?limit=" + self.limit + "&subId=" + self.subId
         url = "{}{}".format(self.DOMAIN, URL)
-        print(url)
+        #print(url)
         response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
         resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
-        print(resdata)
-        return 0
+        #print(resdata)
+        return resdata
 
     def goldbox_coupang(self):
         URL = "/v2/providers/affiliate_open_api/apis/openapi/products/goldbox"+ "?subId=" + self.subId
         url = "{}{}".format(self.DOMAIN, URL)
-        print(url)
+        #print(url)
         response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
         resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
-        print(resdata)
+        #print(resdata)
         return 0
+
+    def create_data(self, contents):
+        now_time = time.time()
+        tm = time.strftime('%Y-%m-%d_%I%M%S',time.localtime(now_time))
+        file_name = 'Data\\' + str(tm) + '.csv'
+        
+        with open(file_name,'w',encoding='utf-8') as f:
+            f.write(contents)
+        return 0
+
+################### Categories ####################
+
+#1001   여성패션
+#1002	남성패션
+#1010	뷰티
+#1011	출산/유아동
+#1012	식품
+#1013	주방용품
+#1014	생활용품
+#1015	홈인테리어
+#1016	가전디지털
+#1017	스포츠/레저
+#1018	자동차용품
+#1019	도서/음반/DVD
+#1020	완구/취미
+#1021	문구/오피스
+#1024	헬스/건강식품
+#1025	국내여행
+#1026	해외여행
+#1029	반려동물용품
+#1030	유아동패션
+###################################################
+
+
+category_list = ["1001","1010","1016","1017","1018","1019"]
 
 
 #/products/goldbox
@@ -58,6 +96,8 @@ keyword_origin = '샤프'
 keyword = urllib.parse.quote(keyword_origin)
 
 Id = "1001"
-#Coupang().search_coupang(keyword)
-#Coupang().best_categories_coupang(Id)
-Coupang().goldbox_coupang()
+#data = Coupang().search_coupang(keyword)
+data_str = Coupang().best_categories_coupang(Id)
+#Coupang().goldbox_coupang()
+data_json = json.loads(data_str)
+Coupang().create_data(str(data_json['data']))
