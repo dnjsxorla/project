f22abc7fd355dfca1faf75802b53919623751f6e
dnjsxorla <32029676+dnjsxorla@users.noreply.github.com>
2022-02-14 18:06:13 +0900

    Update test02.py

--- test02.py
+++ test02.py
@@ -11,12 +11,13 @@ class Coupang:
 
 #Coupang default value Access_ket and Secret_key and etc... 
     def __init__(self):
-        self.access_key = 'X' 
-        self.secret_key = 'X'
-        self.subId = '' 
+        self.access_key = 'x' 
+        self.secret_key = 'x'
+        self.subId = 'AF8339687' 
         self.method = 'GET' 
+        self.limit = '10'
         self.DOMAIN = 'https://api-gateway.coupang.com' 
-
+        
     def generateHmac(self, url): 
         path, *query = url.split("?")
         os.environ["TZ"] = "GMT+0"
@@ -26,13 +27,37 @@ class Coupang:
         return "CEA algorithm=HmacSHA256, access-key={}, signed-date={}, signature={}".format(self.access_key, datetime, signature)
 
     def search_coupang(self, keyword):
-        URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + keyword + "&limit=10" 
+        URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + keyword + "&limit=" + self.limit + "&subId=" + self.subId
         url = "{}{}".format(self.DOMAIN, URL)
         response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
         resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
         print(resdata)
         return 0
 
+    def best_categories_coupang(self, Id):
+        URL = "/v2/providers/affiliate_open_api/apis/openapi/products/bestcategories/"+ Id + "?limit=" + self.limit + "&subId=" + self.subId
+        url = "{}{}".format(self.DOMAIN, URL)
+        print(url)
+        response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
+        resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
+        print(resdata)
+        return 0
 
-keyword = "ipad"
-Coupang().search_coupang(keyword)
+    def goldbox_coupang(self):
+        URL = "/v2/providers/affiliate_open_api/apis/openapi/products/goldbox"+ "?subId=" + self.subId
+        url = "{}{}".format(self.DOMAIN, URL)
+        print(url)
+        response = requests.request(method=self.method, url=url, headers={"Authorization": self.generateHmac(URL),"Content-Type": "application/json;charset=UTF-8"})
+        resdata = json.dumps(response.json(), indent=4, ensure_ascii=False)
+        print(resdata)
+        return 0
+
+
+#/products/goldbox
+keyword_origin = '샤프'
+keyword = urllib.parse.quote(keyword_origin)
+
+Id = "1001"
+#Coupang().search_coupang(keyword)
+#Coupang().best_categories_coupang(Id)
+Coupang().goldbox_coupang()
